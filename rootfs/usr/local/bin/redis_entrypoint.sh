2aec6723960aa74a0c3cfef587e83ea28f6c7437
jacknam <jacknam@naver.com>
2019-09-10 13:16:59 +0900

    # Fix Redis Entrypoint

--- rootfs/usr/local/bin/redis_entrypoint.sh
+++ rootfs/usr/local/bin/redis_entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [ ! -f "/etc/redis/redis.conf" ]; then
+ exit 1
+fi
+
+sysctl -w vm.overcommit_memory=1
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
 chown -R redis:redis /var/mail/redis
+if [ -f "/var/mail/redis/appendonly.aof" ]; then
+ redis-check-aof --fix /var/mail/redis/appendonly.aof
+fi
+
+exit 0
